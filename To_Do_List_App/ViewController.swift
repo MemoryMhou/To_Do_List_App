ead92e89667580a034a61242d2ce1c85c01077bc
MemoryMhou <memorymhou1@gmail.com>
2024-02-25 22:52:02 +0200

    creating_new_items

--- To_Do_List_App/ViewController.swift
+++ To_Do_List_App/ViewController.swift
@@ -25,14 +25,41 @@ class ViewController: UIViewController {
     }
     
     func createItem(name: String){
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
         
+        do {
+            try context.save()
+            
+        }
+        catch {
+            //error
+        }
     }
     
     func deleteItem(item: ToDoListItem){
+        context.delete(item)
         
+        do {
+            try context.save()
+            
+        }
+        catch {
+            //error
+        }
     }
     
-    func updateItem(item: ToDoListItem){
+    func updateItem(item: ToDoListItem, newName: String){
+        item.name = newName
+        
+        do {
+            try context.save()
+            
+        }
+        catch {
+            //error
+        }
         
     }
     
