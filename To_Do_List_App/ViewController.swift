06ce71372989a081a0aae8bee0b27b2c3fbb668b
MemoryMhou <memorymhou1@gmail.com>
2024-02-25 23:26:13 +0200

    TableView

--- To_Do_List_App/ViewController.swift
+++ To_Do_List_App/ViewController.swift
@@ -7,17 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var models = [ToDoListItem]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    title = "Core Data To Do List"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        
         // Do any additional setup after loading the view.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+        
+    }
+    
+    // CORE DATA
     func getAllItems() {
         do{
-            let items = try context.fetch(ToDoListItem.fetchRequest())
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         catch {
             //error
