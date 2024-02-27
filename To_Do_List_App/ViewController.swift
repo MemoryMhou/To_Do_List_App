c116b75de197f897948baca491a8011e7b9ba4f5
MemoryMhou <memorymhou1@gmail.com>
2024-02-27 22:57:21 +0200

    deletion_of_items

--- To_Do_List_App/ViewController.swift
+++ To_Do_List_App/ViewController.swift
@@ -64,6 +64,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "Edit",
+                                      message: "nil",
+                                      preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "cancel", style: .cancel, handler:nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler:{ _ in
+            
+            let alert = UIAlertController(title: "EditItem",
+                                          message: "Edit your item",
+                                          preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "save", style: .cancel, handler:{ [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else{
+                    return
+                }
+                self?.updateItem(item: item, newName: newName)
+            }))
+            self.present (alert, animated: true)
+            
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler:{ [weak self] _ in
+            self?.deleteItem(item: item)
+                             
+                             }))
+        present (sheet, animated: true)
+        
     }
     
     // CORE DATA
